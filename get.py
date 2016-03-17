c48c42efde6f22011c5bdf2002fc86997c2ca5df
Austin Colcord <ajdcolcord@gmail.com>
2016-03-17 14:38:14 -0400

    abstracting get function out of the get_login_page function to take in a given path

--- get.py
+++ get.py
@@ -8,16 +8,17 @@ LOGIN_PAGE_PATH = "/accounts/login/?next=/fakebook.html"
 
 
 def get_login_page():
+    return get(LOGIN_PAGE_PATH)
+
+
+def get(path_name):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # connect the socket to the host
     try:
         sock.connect((HOSTNAME, PORT))
         print "Successfully connected to: " + str(HOSTNAME) + " on port: " + str(PORT)
 
-        sock.send("GET " + str(LOGIN_PAGE_PATH) + "\n") #+
-                #"From: acolcord@ccs.neu.edu\n" +
-                #"User-Agent: HTTPTool/1.0\n")
+        sock.send("GET " + str(path_name) + "\n") #+
 
         return sock.recv(RECV_MESSAGE_SIZE)
 
