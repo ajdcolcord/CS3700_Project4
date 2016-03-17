1ca61bc7b83080e926f3c3dbb75da193521d36dd
Austin Colcord <ajdcolcord@gmail.com>
2016-03-17 13:47:57 -0400

    updates for socket connection, removing webcrawler.py (trash file)

--- webcrawler2.py
+++ webcrawler2.py
@@ -3,7 +3,10 @@ import socket, sys
 HOSTNAME = "fring.ccs.neu.edu"
 PORT = 80
 
+LOGIN_PAGE_PATH = "/accounts/login/?next=/fakebook.html"
+
 def main():
+
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -12,18 +15,20 @@ def main():
         s.connect((HOSTNAME, PORT))
         print "Successfully connected to: " + str(HOSTNAME) + " on port: " + str(PORT)
 
-        send_message(s)
+        get_login_page(s)
 
     except socket.error:
         print "Failed To Connect Socket to Host"
         sys.exit(1)
 
 
-def send_message(socket):
-    socket.send("GET /accounts/login/?next=/fakebook.html\n") #+
+def get_login_page(socket):
+    recv_buffer=""
+    socket.send("GET " + str(LOGIN_PAGE_PATH) + "\n") #+
                 #"From: acolcord@ccs.neu.edu\n" +
                 #"User-Agent: HTTPTool/1.0\n")
-    print socket.recv(1024)
+
+    print socket.recv(5000)
 
 
 
