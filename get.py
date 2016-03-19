42ba6b3a8e7f56b0e10b4aec4a62b73619fad19d
Austin Colcord <ajdcolcord@gmail.com>
2016-03-18 23:01:12 -0400

    Using own GET and POST functions now. Next, need to figure out why forbidden login occurring

--- get.py
+++ get.py
@@ -1,30 +1,54 @@
 import socket, sys
+import select
 
 HOSTNAME = "fring.ccs.neu.edu"
 PORT = 80
-RECV_MESSAGE_SIZE = 5000
-
-LOGIN_PAGE_PATH = "/accounts/login/?next=/fakebook.html"
-
-
-def get_login_page():
-    return get(LOGIN_PAGE_PATH)
+RECV_MESSAGE_SIZE = 1024
+USERNAME = "001196344"
+PASSWORD = "7L3AD6ZH"
 
 
-def get(path_name):
+LOGIN_PAGE_PATH = "/accounts/login/?next=/fakebook/"
+
+GET_LOGIN_STRING = "GET /accounts/login/?next=/fakebook/ HTTP/1.1 \nHost: fring.ccs.neu.edu\nUser-Agent: HTTPTool/1.1 \n \n"
+
+
+def send_and_receive(message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         sock.connect((HOSTNAME, PORT))
-        print "Successfully connected to: " + str(HOSTNAME) + " on port: " + str(PORT)
-
-        sock.send("GET " + str(path_name) + "\n") #+
-
-        return sock.recv(RECV_MESSAGE_SIZE)
-
+        sock.send(message)
+        result = ""
+        while True:
+            recv = sock.recv(RECV_MESSAGE_SIZE)
+            if recv:
+                result += recv
+            else:
+                break
+        return result
     except socket.error:
-        print "Failed To Connect Socket to Host"
+        print "Could not connect to host" + str(HOSTNAME)
         sys.exit(1)
 
 
-print get_login_page()
+def get_login_page():
+    get_str = "GET /accounts/login/?next=/fakebook/ HTTP/1.1\n" \
+              "Host: fring.ccs.neu.edu\n" \
+              "User-Agent: HTTPTool/1.1\n\n"
+    return send_and_receive(get_str)
+
+
+def post_login(path_name, username, password, mwtoken):
+
+    post_message = "POST " + str(path_name) + " HTTP/1.0 \n" \
+                                         "Content-Type: application/x-www-form-urlencoded\n\n" \
+                                         "username=" + str(username) + "&password=" + str(password) + \
+                                         "&csrfmiddlewaretoken=" + str(mwtoken) + "&next=/fakebook/"
+
+    return send_and_receive(post_message)
+
+
+
+#print get_login_page()
+#print get_login_page()
+#print post_login(LOGIN_PAGE_PATH, USERNAME, PASSWORD, 0)
